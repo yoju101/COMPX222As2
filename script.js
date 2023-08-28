ff1065d9f7b416f66ff16263a62da1505e21d0f6
yonev <yv4.student.waikato@gmail.com>
2023-08-28 16:31:12 +1200

    added older code

--- script.js
+++ script.js
@@ -72,3 +72,95 @@ function startQuestionnaire() {
     cholesterolSection.classList.add('visible');
 
   }
+
+
+
+  
+// function startQuestionnaire() {
+//   //gets the id and hides the content in that div
+//     var welcomePage = document.getElementById('welcomePage');
+//      welcomePage.classList.add('hidden');
+//      var tool = document.getElementById('ageGenderSection');
+//       tool.classList.add('visible');
+//   }
+
+//   //to get the amount from the slider bar and display it to the users HDLValue
+//   function SmokeQusestion() {
+//     const welcomePage = document.getElementById('smokeSection');
+//     const HDLQ = document.getElementById('smokeSection');
+    
+//     welcomePage.classList.add('hidden');
+//     HDLQ.classList.remove('hidden');
+//   }
+
+//   //to get the amount from the slider bar and display it to the users HDLValue
+//   function HDLQusestion() {
+//     const welcomePage = document.getElementById('HDLSection');
+//     const HDLQ = document.getElementById('HDLSection');
+    
+//     welcomePage.classList.add('hidden');
+//     HDLQ.classList.remove('hidden');
+//   }
+  
+//   function cholesterolQuestion() {
+//     const HDLSection = document.getElementById('HDLSection');
+//     const cholesterolSection = document.getElementById('cholesterolSection');
+    
+//     HDLSection.classList.add('hidden');
+//     cholesterolSection.classList.remove('hidden');
+//   }
+//   function goBack(sectionId) {
+//     const currentSection = document.getElementById(sectionId);
+//     currentSection.classList.add('hidden');
+//     ageGenderSection.classList.remove('hidden');
+//   }
+//   document.getElementById('HDL').addEventListener('input', function () {
+//     const selectedHDL = document.getElementById('HDL').value;
+//     document.getElementById('HDLValue').textContent = selectedHDL;
+//   });
+
+//   //to get the amount from the slider bar and display it to the users age
+//   document.getElementById('age').addEventListener('input', function () {
+//     const selectedAge = document.getElementById('age').value;
+//     document.getElementById('ageValue').textContent = selectedAge;
+//     // Display a pop-up notification
+//     alert(`You have chosen ${selectedGender} gender.`);
+//   });
+
+//   //to get the amount from the slider bar and display it to the users age
+//   document.getElementById('gender').addEventListener('input', function () {
+//     const selectedAge = document.getElementById('gender').value;
+//     document.getElementById('ageValue').textContent = selectedGender;
+//     // Display a pop-up notification
+//     alert(`You have chosen ${selectedGender} gender.`);
+//   });
+
+// //   //to get the amount from the and display it to the users age
+// //   document.getElementById('HDL').addEventListener('input', function () {
+// //     const selectedHDL = document.getElementById('HDL').value;
+// //     document.getElementById('HDLValue').textContent = selectedHDL;
+// //   });
+  
+//   document.getElementById('cholesterol').addEventListener('input', function () {
+//     const selectedCholesterol = document.getElementById('cholesterol').value;
+//     document.getElementById('cholesterolValue').textContent = selectedCholesterol;
+//   });
+
+//   function nextPage() {
+//     // Get the selected gender value
+//     const selectedGender = document.getElementById('gender').value;
+  
+    
+//   }
+//   function submitForm() {
+//     const selectedGender = document.getElementById('gender').value;
+//     const selectedAge = document.getElementById('age').value;
+//     const selectedHDL = document.getElementById('HDL').value;
+//     const selectedCholesterol = document.getElementById('cholesterol').value;
+//     const selectedSmoke = document.getElementById('smoke').value;
+  
+//     alert(`You have chosen:\nGender: ${selectedSmoke}\nSmoke:${selectedAge}\nAge: ${selectedGender}\nHDL: ${selectedHDL}\nCholesterol: ${selectedCholesterol}`);
+// }
+// // the page is a mess need to clean up need to look at lecture slides and see how thay want it this is from GPT 
+  
+  
